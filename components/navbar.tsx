49bc0ce680a3493620d46c51b796acdfddf9c0f2
Your Name <your.email@example.com>
2026-02-12 19:37:36 +1200

    updated all the details for ictaca 2026

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -54,7 +54,7 @@ export default function Navbar() {
               animate={{ x: 0, opacity: 1 }}
               transition={{ duration: 0.5, delay: 0.1 }}
             >
-              ICTACA-2025
+              {conferenceData.shortName}
             </motion.span>
           </Link>
 
